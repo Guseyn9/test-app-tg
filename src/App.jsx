5d063da118b9682768dd21140bc6836890b41f0f
Guseyn <117921674+Guseyn9@users.noreply.github.com>
2025-08-14 19:49:20 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [user, setUser] = useState(null);
+  const [user1, setUser1] = useState();
 
   useEffect(() => {
   if (window.Telegram && window.Telegram.WebApp) {
     const tg = window.Telegram.WebApp;
     tg.ready();
     setUser(tg.initDataUnsafe?.user || null);
+    setUser1(tg.initDataUnsafe?.user)
     console.log("initDataUnsafe:", tg.initDataUnsafe);
 
     tg.MainButton.setText("Закрыть приложение");
@@ -26,7 +28,7 @@ function App() {
           Вы зашли как <b>{user.first_name}</b> (@{user.username})
         </p>
       ) : (
-        <p>Данные о пользователе не получены Данные о пользователе: {JSON.stringify(user, null, 2)}</p>
+        <p>Данные о пользователе не получены Данные о пользователе: {JSON.stringify(user1, null, 2)}</p>
       )}
     </div>
   );
